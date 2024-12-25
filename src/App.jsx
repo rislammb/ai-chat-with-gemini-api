f1588aa80e4ea513f97611fd6a03ff1a4d95264c
Rayhanul Islam <rislammb@gmail.com>
2024-12-25 08:22:03 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="app">
+      <section className={"search-section"}>
+          <p>What do yoy want to know?
+          <button className={"surprise"}>Surprise me</button>
+          </p>
+      </section>
+    </div>
+  )
+}
+
+export default App
